ccf483223c38e113fa07718d47b2f0acd6ab0283
JaneLytv <jane.lytv@gmail.com>
2015-05-28 15:28:05 -0400

    just some markup

--- index.php
+++ index.php
@@ -1,18 +1,58 @@
 <?php //index.php is the last resort template, if no other templates match ?>
 <?php get_header(); ?>
 
-<div class="main">
+<div class="main hero">
 
-  <div class="container">
 
-    <div class="content">
 
 <!-- THE BELOW IS A LOOP WHICH GETS CONTENT FROM ALL POSTS AND PUBLISHES IT TO THE FRONT PAGE [jane] -->
+<?php //get_template_part( 'loop', 'index' );	?>
 
-    		<!-- <?php //get_template_part( 'loop', 'index' );	?>   -->
-    </div> <!--/.content -->
+<!-- +++++THIS IS THE BIG HERO IMAGE PAGE WHICH PULLS IN FROM PAGES+++++ -->
 
-  </div> <!-- /.container -->
+<div class="page-full-img">
+	<h2 class="mib"></h2>
+	<p class="mib"></p>
+</div>
+<div class="page-full-img">
+	<h2 class="mib"></h2>
+	<p class="mib"></p>
+</div>
+<div class="page-full-img">
+	<h2 class="mib"></h2>
+	<p class="mib"></p>
+</div>
+
+<!-- six  posts which are also flexed [jane] -->
+
+<div class="main-flex">
+
+	<div class="main-box">
+		<p class="flex-text"></p>
+	</div>
+
+	<div class="main-box">
+		<p class="flex-text"></p>
+	</div>
+
+	<div class="main-box">
+		<p class="flex-text"></p>
+	</div>
+
+	<div class="main-box">
+		<p class="flex-text"></p>
+	</div>
+
+	<div class="main-box">
+		<p class="flex-text"></p>
+	</div>
+
+	<div class="main-box">
+		<p class="flex-text"></p>
+	</div>
+
+</div>
+
 </div> <!-- /.main -->
 
 <?php get_footer(); ?>
