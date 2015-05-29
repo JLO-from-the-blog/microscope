a15f83a0d5b6301ce85a741b397810c722e1560d
MarloHwang <pizzaparty@gmail.com>
2015-05-29 14:25:31 -0400

    index hero images + php functions

--- page.php
+++ page.php
@@ -2,10 +2,16 @@
 
 <div class="main">
   <div class="container">
-  <p>I am editing ok? PAGE.php</p>
+  <p>PAGE.php</p>
     <div class="content">
       <?php // Start the loop ?>
       <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+<?php 
+if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+  the_post_thumbnail('full');
+} 
+?>
 
         <h2><?php the_title(); ?></h2>
         <?php the_content(); ?>
